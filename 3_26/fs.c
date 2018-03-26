b1d23178e5c734f31d2aec51656d1c054b7c0cbf
mgh3326 <mgh3326@naver.com>
2018-03-26 15:23:54 +0900

    update

---
+++ 3_26/fs.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "fs.h"
+
+void FileSysInit(void)
+{
+
+}
+void SetInodeBitmap(int inodeno)
+{
+
+}
+
+
+void ResetInodeBitmap(int inodeno)
+{
+
+}
+
+
+void SetBlockBitmap(int blkno)
+{
+
+}
+
+
+void ResetBlockBitmap(int blkno)
+{
+
+}
+
+
+void PutInode(int inodeno, Inode* pInode)
+{
+
+}
+
+
+void GetInode(int inodeno, Inode* pInode)
+{
+
+}
+
+
+int GetFreeInodeNum(void)
+{
+
+}
+
+
+int GetFreeBlockNum(void)
+{
+
+}
+
+
+
+
+
+
