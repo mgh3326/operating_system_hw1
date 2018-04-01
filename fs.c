f36701217b8e4873f008a4ad482c5c58bd908159
mgh3326 <mgh3326@naver.com>
2018-04-01 16:18:00 +0900

    update

--- fs.c
+++ fs.c
@@ -27,8 +27,6 @@ void SetInodeBitmap(int inodeno)
     //DevOpenDisk();
     DevReadBlock(INODE_BITMAP_BLK_NUM, buf);
     buf[inodeno / 8] |= 1 << (7 - (inodeno % 8)); // if(inodeno % 8 ==0) -> buf[inodeno/8] |= 1000000
-
-    // printf("second : %s\n", buf);
     DevWriteBlock(INODE_BITMAP_BLK_NUM, buf);
     free(buf);
 }
@@ -39,8 +37,6 @@ void ResetInodeBitmap(int inodeno)
     //DevOpenDisk();
     DevReadBlock(INODE_BITMAP_BLK_NUM, buf);
     buf[inodeno / 8] &= ~(1 << (7 - (inodeno % 8)));
-
-    // printf("second : %s\n", buf);
     DevWriteBlock(INODE_BITMAP_BLK_NUM, buf);
     free(buf);
 }
@@ -52,25 +48,16 @@ void SetBlockBitmap(int blkno)
     //DevOpenDisk();
     DevReadBlock(BLOCK_BITMAP_BLK_NUM, buf);
     buf[blkno / 8] |= 1 << (7 - (blkno % 8));
-
-    // printf("second : %s\n", buf);
     DevWriteBlock(BLOCK_BITMAP_BLK_NUM, buf);
     free(buf);
 }
 
 void ResetBlockBitmap(int blkno)
 {
-    // //DevOpenDisk();
-
     char *buf = malloc(BLOCK_SIZE);
     //DevOpenDisk();
     DevReadBlock(BLOCK_BITMAP_BLK_NUM, buf);
-    int num_inode = blkno / 8;   //inode 가 몇번째 배열에 있는지를 나타냅니다.
-    int index_inode = blkno % 8; //inode의 index를 가르킵니다.
     buf[blkno / 8] &= ~(1 << (7 - (blkno % 8)));
-
-    // printf("second : %d\n", buf[0]);
-
     DevWriteBlock(BLOCK_BITMAP_BLK_NUM, buf);
     free(buf);
 }
@@ -81,13 +68,9 @@ void PutInode(int inodeno, Inode *pInode)
     char *buf = malloc(BLOCK_SIZE);
     //DevOpenDisk();
     DevReadBlock((inodeno / 8) + INODELIST_BLK_FIRST, buf);
-    // pInode->allocBlocks--;
-    // memcpy(buf+(blkno % 8)*64, (void *)&pInode->type, BLOCK_SIZE/8);
     memcpy(buf + (inodeno % 8) * sizeof(Inode), pInode, sizeof(Inode));
     DevWriteBlock((inodeno / 8) + INODELIST_BLK_FIRST, buf);
     free(buf);
-
-    //   memcpy((void *)&pInode->type, (void *)&buf+(blkno % 8)*64, BLOCK_SIZE/8);
 }
 
 void GetInode(int inodeno, Inode *pInode)
@@ -95,58 +78,21 @@ void GetInode(int inodeno, Inode *pInode)
 
     char *buf = malloc(BLOCK_SIZE);
     //DevOpenDisk();
-
-    //3번째 블록에 0~7
-    //4             8~15
-    //5             16~23
-    //6             24~31
     DevReadBlock((inodeno / 8) + INODELIST_BLK_FIRST, buf);
-    // for(int i=0;i<512;i++)
-    // {
-    //     printf("%d : %d \n",i,pInode[i].type);
-    // }
-
-    //memcpy((void *)&pInode->type, (void *)&buf+(blkno % 8)*64, BLOCK_SIZE/8);
     memcpy(pInode, buf + (inodeno % 8) * sizeof(Inode), sizeof(Inode));
-
-    // pInode->allocBlocks = 0;
-    // pInode->allocBlocks++;
     free(buf);
-
-    // pInode->type=?;
-
-    // pInode->blockPointer[NUM_OF_BLK_PTR]; // Direct block pointers
 }
 
 int GetFreeInodeNum(void)
 {
-    //DevOpenDisk();
-
     char *buf = malloc(BLOCK_SIZE);
     ////DevOpenDisk();
     DevReadBlock(INODE_BITMAP_BLK_NUM, buf);
 
     for (int i = 0; i < BLOCK_SIZE; i++)
     {
-        //if pBlock[i] == 11111111, continue
-        if (buf[i] == -1)
+        if (buf[i] == -1) // if 11111111 => continue
             continue;
-
-        // for (int j = 7; j >= 0; j--)
-        // {
-        //     if (!(buf[i] >> j & 1)) // >> 연산자가 & 보다 우선순위가 높구나
-        //     {
-        //         return (i * 8) + (7 - j);
-        //     }
-        // }
-        // for (int j = 0; j < 8; j++)
-        // {
-        //     if ((buf[i] >> (7 - j) & 1) == 0) // >> 연산자가 & 보다 우선순위가 높구나
-        //     {
-        //         free(buf);
-        //         return (i * 8) + j;
-        //     }
-        // }
         for (int j = 0; j < 8; j++)
         {
             if ((buf[i] << j & 128) == 0) // >> 연산자가 & 보다 우선순위가 높구나
@@ -157,71 +103,19 @@ int GetFreeInodeNum(void)
         }
     }
     free(buf);
-    return -1; //실패 했을 경우?
-    // int binary[20] = { 0, };
-    // int position = 0;
-    // int index;
-    // for(int i=0;i<BLOCK_SIZE;i++)
-    // {
-    //      int decimal = buf[i];
-
-    //     while (1)
-    //     {
-    //         binary[position] = decimal % 2;    // 배열에 나머지 저장
-    //         decimal = decimal / 2;             // 2로 나눈 몫을 저장
-
-    //         position++;    // 자릿수 변경
-    //          if (decimal == 0)    // 몫이 0이 되면 반복을 끝냄
-    //          {
-    //             index=0;
-    //             break;
-    //          }
-
-    //     }
-    //     for (int j = position - 1; j >= 0; j--)
-    //         {
-    //             //printf("%d", binary[j]);
-    //             if(binary[j]==0)
-    //                 {
-    //                         free(buf);
-
-    //                     return index;
-    //                 }
-    //             index++;
-    //         }
-    // }
-    //     free(buf);
-
-    // return -1; //실패할 경우
+    return -1; //실패 했을 경우
 }
 
 int GetFreeBlockNum(void)
 {
     char *buf = malloc(BLOCK_SIZE);
-    ////DevOpenDisk();
     DevReadBlock(BLOCK_BITMAP_BLK_NUM, buf);
 
     for (int i = 0; i < BLOCK_SIZE; i++)
     {
-        //if pBlock[i] == 11111111, continue
-        if (buf[i] == -1)
+        if (buf[i] == -1) // if 11111111 => continue
             continue;
 
-        // for (int j = 7; j >= 0; j--)
-        // {
-        //     if (!(buf[i] >> j & 1))
-        //     {
-        //         return (i * 8) + (7 - j);
-        //     }
-        // }
-        // for (int j = 0; j < 8; j++)
-        // {
-        //     if ((buf[i] >> (7 - j) & 1) == 0) // >> 연산자가 & 보다 우선순위가 높구나
-        //     {
-        //         free(buf);
-        //         return (i * 8) + j;
-        //     }
-        // }
         for (int j = 0; j < 8; j++)
         {
             if ((buf[i] << j & 128) == 0) // >> 연산자가 & 보다 우선순위가 높구나
@@ -232,41 +126,5 @@ int GetFreeBlockNum(void)
         }
     }
     free(buf);
-    return -1; //실패 했을 경우?
-    // int binary[20] = { 0, };
-    // int position = 0;
-    // int index;
-    // for(int i=0;i<BLOCK_SIZE;i++)
-    // {
-    //      int decimal = buf[i];
-
-    //     while (1)
-    //     {
-    //         binary[position] = decimal % 2;    // 배열에 나머지 저장
-    //         decimal = decimal / 2;             // 2로 나눈 몫을 저장
-
-    //         position++;    // 자릿수 변경
-    //          if (decimal == 0)    // 몫이 0이 되면 반복을 끝냄
-    //          {
-    //             index=0;
-    //             break;
-    //          }
-
-    //     }
-    //     for (int j = position - 1; j >= 0; j--)
-    //         {
-    //             //printf("%d", binary[j]);
-    //             if(binary[j]==0)
-    //             {
-    //                             free(buf);
-
-    //                 return index;
-    //             }
-
-    //             index++;
-    //         }
-    // }
-    //             free(buf);
-
-    // return -1; //실패할 경우
+    return -1; //실패 했을 경우
 }
