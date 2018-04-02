7cfce3fe0f0363ae11e66b271300ae97e049e011
mgh3326 <mgh3326@naver.com>
2018-04-02 14:24:45 +0900

    update

---
+++ cmake/os_hw1/cmake-build-debug/CMakeFiles/os_hw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_hw1.dir/disk.c.o"
+  "CMakeFiles/os_hw1.dir/fs.c.o"
+  "CMakeFiles/os_hw1.dir/testcase.c.o"
+  "os_hw1.pdb"
+  "os_hw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/os_hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/os_hw1/validate.h
@@ -0,0 +1,7 @@
+#ifndef __VALIDATE_H__
+#define __VALIDATE_H__
+#include "fs.h"
+int power(int val);
+int vGetBitValue(int blkno, int bitno);
+void vGetInodeValue(int inodeno, Inode *pTmpInode);
+#endif
